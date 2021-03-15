e58d867be6c1221fdc1d16534e70e1277301d251
purplecoder04 <purple_lover_04@msn.com>
2021-03-15 03:06:35 +0000

    found to ways

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 #Write your code below this line 👇
+def prime_checker(number):
+  is_prime = True
+  for i in range(2, number):
+    if number%i == 0:
+      is_prime = False
+  if is_prime:
+    print("it's a prime number.")
+  else:
+    print("it's not a prime number.")
+  # if number == 2:
+  #   print("number is prime")
+  # elif number == 3:
+  #   print("number is prime")
+  # elif number %2 == 0 or number %3 == 0:
+  #   print("number is not prime")
+  # else:
+  #   print("number is prime")
+
 
 
 
